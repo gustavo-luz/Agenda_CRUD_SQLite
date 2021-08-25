7c6d06a1c1b9a49b32eeee8ab00d50e37deb589a
gustavo-luz <gustavo.luz.unb@gmail.com>
2021-08-25 11:44:41 -0300

    added funcs at all options

--- finalcode.py
+++ finalcode.py
@@ -24,12 +24,12 @@ def db_insert(name,phone,email,twitter,instagram):
     """.format(name,phone,email,twitter,instagram)
 
 
-#update TODO update based on dif where's
+#update TODO update based on dif where's (email, twitter, insta, deixar o usuario escolher)
 @commit_close
 def db_update(name,email):
     return"""
-    UPDATE users SET name='{}' WHERE email='{}'
-    """.format(name,email)
+    UPDATE users SET email='{}' WHERE name='{}'
+    """.format(email,name)
 
 #delete
 @commit_close
@@ -40,7 +40,7 @@ def db_delete(name):
 
 
 #read
-#read consulta
+#read consulta individual
 def db_select(data,field):
     con=sqlite3.connect('agenda.db')
     cur=con.cursor()
@@ -53,42 +53,79 @@ def db_select(data,field):
     con.close()
     return data
 
+#read consulta total
+def db_select_all():
+    con=sqlite3.connect('agenda.db')
+    cur=con.cursor()
+    sql= """SELECT *
+    FROM users
+    """
+
+    cur.execute(sql)
+    data=cur.fetchall()
+    con.close()
+    return data
+
+
 if __name__ == '__main__':
     option=True
 
 while option:
+    
+
     print ("""
-    1.Inserir Contato
-    2.Consultar Contato
-    3.Remover Contato
-    4.Alterar Contato
+    Menu:\n
+    1. Inserir Contato
+    2. Consultar Contato
+    3. Remover Contato
+    4. Alterar Contato
     5. Gerar Relatório
     6. Sair
     """)
-    option=input("Escolha uma opção do Menu ")
+    option=input("Escolha uma opção do Menu: ")
 
 #TODO criar opção de cadastrar mais de um contato, pedir o input do numero e repertir os pedidos de input quantas vezes precisar
     if option=="1":
-        nome=input("Informe o nome ")
-        telefone=input("Informe o telefone ")
-        email=input("Informe o e-mail ")
-        twitter=input("Informe a conta do twitter ")
-        instagram=input("Informe a conta do instagram ")
+        nome=input("Informe o nome: ")
+        telefone=input("Informe o telefone: ")
+        email=input("Informe o e-mail: ")
+        twitter=input("Informe a conta do twitter: ")
+        instagram=input("Informe a conta do instagram: ")
         db_insert(nome,telefone,email,twitter,instagram)
+        #TODO COLOCAR TABULAÇÃO DESTACANDO O PRINT: https://www.educba.com/python-print-table/
         print("\n Contato inserido:","nome:",nome,'\t',"telefone:",telefone,'\t',"email:",email,'\t',"twitter:",twitter,'\t',"instagram:",instagram) 
 
 
     elif option=="2":
-        print("\n Contato encontrado") 
+        nome=input("Qual nome deseja encontrar? ")
+        #TODO arrumar a consulta pra não ter que ser o id
+        consulta=(db_select('id',nome))
+        #TODO COLOCAR TABULAÇÃO DESTACANDO O PRINT E COM OS HEADERS
+        print("\n Contato encontrado",consulta) 
+
     elif option=="3":
-        print("\n Contato Removido") 
+        nome=input("Qual nome deseja remover? ")
+        db_delete(nome)
+        #TODO COLOCAR TABULAÇÃO DESTACANDO O PRINT E COM OS HEADERS
+        print(nome,"removido(a) da agenda com sucesso") 
+
     elif option=="4":
+        nome= input("Qual usuário deseja alterar? ")
+        mail=input("Qual o novo email? ")
+        db_update(nome,mail)
         print("\n Contato Alterado") 
+
     elif option=="5":
-        print("\n Relatório Gerado")
-        #TODO SELECT * FROM USERS 
+        report=db_select_all()
+        print("\n Relatório Gerado:")
+        #TODO COLOCAR TABULAÇÃO DESTACANDO O PRINT E COM OS HEADERS
+        pprint(report)
+        
+
     elif option=="6":
-        print("\n Até Logo")
+        
+        pprint("\n Até Logo")
         break 
+
     elif option !="":
-        print("\n Escolha outra opção") 
+        print("\n Opção inválida, escolha outra opção") 
